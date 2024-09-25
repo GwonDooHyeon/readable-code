6386a20e08a1616635ce3930cd93d0a7ba714b4d
GwonDooHyeon <secure3141@gmail.com>
2024-09-25 14:09:58 +0900

    다형성 활용

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,35 +1,22 @@
 package cleancode.minesweeper.tobe;
 
-import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.config.GameConfig;
 import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
-import cleancode.minesweeper.tobe.io.InputHandler;
-import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameLevel gameLevel = new VeryBeginner();
-        InputHandler inputHandler = new ConsoleInputHandler();
-        OutputHandler outputHandler = new ConsoleOutputHandler();
-
-        Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
+        GameConfig gameConfig = new GameConfig(
+                new VeryBeginner(),
+                new ConsoleInputHandler(),
+                new ConsoleOutputHandler()
+        );
+        
+        Minesweeper minesweeper = new Minesweeper(gameConfig);
         minesweeper.initialize();
         minesweeper.run();
     }
-
-    /**
-     * DIP (Dependency Inversion Principle)
-     * - 고수준 모듈은 저수준 모듈에 의존해서는 안된다.
-     *  대신, 둘 다 추상화에 의존하여야 한다.
-     * - 추상화는 구체적인 것에 의존하지 말아야 한다.
-     *  구체적인 클래스가 아닌, 추상화 된 인터페이스나 클래스를 사용해야 한다.
-     *
-     * DI (Dependency Injection) - 의존성 주입
-     *
-     * IOC(Inversion of Control) - 제어의 역전
-     *
-     */
 
 }
