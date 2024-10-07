162bae0d7c8cf8b7c7db92c58efed008a122a7dc
GwonDooHyeon <secure3141@gmail.com>
2024-10-08 07:41:29 +0900

    중복 메소드 추출

--- src/main/java/cleancode/studycafe/tobe/io/OutputHandler.java
+++ src/main/java/cleancode/studycafe/tobe/io/OutputHandler.java
@@ -46,7 +46,7 @@ public class OutputHandler {
         System.out.println();
         System.out.println("이용 내역");
         System.out.println("이용권: " + selectedPass.display());
-        if (lockerPass!=null) {
+        if (lockerPass != null) {
             System.out.println("사물함: " + lockerPass.display());
         }
 
@@ -56,7 +56,7 @@ public class OutputHandler {
             System.out.println("이벤트 할인 금액: " + discountPrice + "원");
         }
 
-        int totalPrice = selectedPass.getPrice() - discountPrice + (lockerPass!=null ? lockerPass.getPrice():0);
+        int totalPrice = selectedPass.getPrice() - discountPrice + (lockerPass != null ? lockerPass.getPrice() : 0);
         System.out.println("총 결제 금액: " + totalPrice + "원");
         System.out.println();
     }
